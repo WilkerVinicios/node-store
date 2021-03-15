7ad578559c0d20546b0ceba2fe5265ff972c2568
WilkerVinicios <44215818+WilkerVinicios@users.noreply.github.com>
2021-03-14 21:04:49 -0300

    Criado Model e Schema de Products

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Conexão ao banco
-mongoose.connect('mongodb://wilker:140396@localhost:27017/admin')
+mongoose.connect('mongodb://wilker:140396@localhost:27017/admin');
 
 // Carregar as Rotas
 const indexRoute = require('./routes/index-route');
